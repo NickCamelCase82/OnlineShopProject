fcb2605144460e1269bc4a7dac6ef38f8d19200b
Nick <nick.egorenkov@gmail.com>
2022-07-23 20:11:03 -0700

    Connected to database, connection checked

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const dbCheck = require("../server/db/dbConnectionCheck");
 const PORT = process.env.PORT || 7000;
 
 const app = express();
+
+dbCheck();
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
