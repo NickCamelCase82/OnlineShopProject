afa567d65b43ad09a23605ceb3586f31b900de10
Nick <nick.egorenkov@gmail.com>
2022-07-24 21:46:50 -0700

    Routers created

--- server/app.js
+++ server/app.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
+const cors = require("cors");
 const PORT = process.env.PORT || 7000;
+
+const router = require("./routes/index");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
 const start = async () => {
   try {
