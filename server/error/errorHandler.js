7215015d8665a400c12af1bd791bbb7936eff4f8
Nick <nick.egorenkov@gmail.com>
2022-07-24 22:38:11 -0700

    errorHandler added

---
+++ server/error/errorHandler.js
@@ -0,0 +1,19 @@
+class ErrorHandler extends Error {
+  constructor(status, message) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+
+  static badRequest(message) {
+    return new ErrorHandler(404, message);
+  }
+
+  static internal(message) {
+    return new ErrorHandler(500, message);
+  }
+
+  static forbidden(message) {
+    return new ErrorHandler(403, message);
+  }
+}
