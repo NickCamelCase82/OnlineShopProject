3d28d08ecba1c2d5cff7c47f53cb6139b120130a
Nick <nick.egorenkov@gmail.com>
2022-07-26 16:21:08 -0700

    Working on device controller, type and brand done

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const sequelize = require("./db");
 const models = require("./models/models");
 const errorHandler = require("./middleware/errorHandlerMiddleware");
 const cors = require("cors");
+const path = require("path");
+const fileUpload = require("express-fileupload");
 const PORT = process.env.PORT || 7000;
 
 const router = require("./routes/index");
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
 
 app.use(errorHandler);
--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,7 +1,50 @@
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const errorHandler = require("../error/errorHandler");
+const { type } = require("os");
+
 class DeviceController {
-  async create(req, res) {}
-  async getAll(req, res) {}
-  async getOne(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", fileName));
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        img: fileName,
+      });
+      return res.json(device);
+    } catch (e) {
+      next(errorHandler.badRequest(e.message));
+    }
+  }
+  async getAll(req, res) {
+    const { brandID, typeId } = req.query;
+    let devices;
+    if (!brandID && !typeId) {
+      devices = await Device.findAll();
+    }
+    if (!brandID && typeId) {
+      devices = await Device.findAll({ where: { typeId } });
+    }
+    if (brandID && !typeId) {
+      devices = await Device.findAll({ where: { brandID } });
+    }
+    if (brandID && typeId) {
+      devices = Device.findAll({ where: { brandID, typeId } });
+    }
+    return res.json(devices);
+  }
+  async getOne(req, res) {
+    const { oneName } = req.param.id;
+    const oneDevice = await Device.findOne({ where: oneName });
+    return res.json(oneDevice);
+  }
 }
 
 module.exports = new DeviceController();
