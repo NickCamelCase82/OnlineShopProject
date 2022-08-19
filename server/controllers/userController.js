bbd5dd7ff7a235fb930027831c2d0996498fa719
Nick <nick.egorenkov@gmail.com>
2022-08-18 20:30:24 -0700

    login function corrected

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,38 +1,35 @@
 const errorHandler = require("../error/errorHandler");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const {User, Basket} = require('../models/models');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
 
 const generateJwt = (id, email, role) => {
-  return jwt.sign (
-    {id, email, role}, 
-    process.env.SECRET_KEY,
-    {expiresIn: '24h'}
-  )
-}
-
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
   async registration(req, res, next) {
-    const {email, password, role} = req.body
+    const { email, password, role } = req.body;
     if (!email || !password) {
       return next(errorHandler.badRequest("Missing email and/or password"));
     }
-    const checkExistingEmail = await User.findOne({where: {email}});
-      if (checkExistingEmail) {
-        return next(errorHandler.badRequest("This email already exists")); 
-      }
+    const checkExistingEmail = await User.findOne({ where: { email } });
+    if (checkExistingEmail) {
+      return next(errorHandler.badRequest("This email already exists"));
+    }
     const hashPassword = await bcrypt.hash(password, 5);
-    const user = await User.create({email, role, password: hashPassword});
-    const basket = await Basket.create({userId: user.id});  
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
     const token = generateJwt(user.id, user.email, user.role);
-    
-    return res.json({token});  
+
+    return res.json({ token });
   }
 
   async login(req, res, next) {
-    const {email, password} = req.body;
-    const user = User.findOne({where: {email}});
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
     if (!user) {
       return next(errorHandler.badRequest("User doesn't exist"));
     }
@@ -41,7 +38,7 @@ class UserController {
       return next(errorHandler.badRequest("Wrong password"));
     }
     const token = generateJwt(user.id, user.password, user.role);
-    return res.json({token});
+    return res.json({ token });
   }
 
   async check(req, res, next) {
