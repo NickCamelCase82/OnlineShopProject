47dd9c79a741a61c0d99d1f70c91dabf60ad77cf
Nick <nick.egorenkov@gmail.com>
2022-08-24 13:43:18 -0700

    NavBar authorization

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -3,24 +3,43 @@ import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 import { Context } from "../index";
+import { SHOP_ROUTE } from "../utils/consts";
+import { Button } from "react-bootstrap";
+import { observer } from "mobx-react-lite";
 
-export default function NavigationBar() {
+const NavigationBar = observer(() => {
   const { user } = useContext(Context);
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          <NavLink style={{ color: "white" }} to={SHOP_ROUTE}>
+            OnlineShop
+          </NavLink>
+          {user.isAuth ? (
+            <Nav className="ml-auto" style={{ color: "white" }}>
+              <Button variant={"outline-light"}>Logout</Button>
+              <Button variant={"outline-light"}>Admin</Button>
+            </Nav>
+          ) : (
+            <Nav className="ml-auto" style={{ color: "white" }}>
+              <Button
+                variant={"outline-light"}
+                onClick={() => user.setIsAuth(true)}
+              >
+                Login
+              </Button>
+              <Button variant={"outline-light"}>Registration</Button>
+            </Nav>
+          )}
         </Container>
       </Navbar>
       <br />
     </div>
   );
-}
+});
+
+export default NavigationBar;
