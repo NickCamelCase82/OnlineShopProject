79072df8419ace1b045f8f5def577773288e773c
Nick <nick.egorenkov@gmail.com>
2022-08-24 10:53:44 -0700

    NavBar initialized

---
+++ client/src/components/NavigationBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useContext } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Context } from "../index";
+
+export default function NavigationBar() {
+  const { user } = useContext(Context);
+
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+    </div>
+  );
+}
